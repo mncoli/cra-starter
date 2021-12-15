29dba9391a59ab05887bb31deff70953e80b3ed9
Nicoli <mncoli@3c22fbaabef6.ant.amazon.com>
2021-12-15 13:38:01 +0000

    update

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import { Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import awsExports from './aws-exports';
+import React from 'react';
+Amplify.configure(awsExports);
+
 function App({ signOut, user }) {
   return (
     <div>
       <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
+      <button onClick={() => Auth.federatedSignIn()}>Sign In</button>
       </div>
   );
 }
